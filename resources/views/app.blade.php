1a1d77f841dba3893938de79217ea5ee927ad488
Francisco Madeira <xico2k@gmail.com>
2021-12-01 20:36:21 +0000

    feat: Add `favicon.png`.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Francisco Madeira</title>
+    <link rel="icon" type="image/png" href="{{ asset('/favicon.png') }}" />
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
     <meta name="referrer" content="no-referrer-when-downgrade" />
