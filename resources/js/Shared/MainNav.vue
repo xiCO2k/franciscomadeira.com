d9bad8c2119822c8ccb30e7521d4d11037cc1ae2
Francisco Madeira <xico2k@gmail.com>
2021-12-02 03:18:44 +0000

    fix: Classes on the `MainNav`.

--- resources/js/Shared/MainNav.vue
+++ resources/js/Shared/MainNav.vue
@@ -2,7 +2,7 @@
     <nav class="flex space-x-4 sm:space-x-8 text-gray-400 text-sm sm:text-base">
         <Link
             :href="route('about')"
-            class="border-b-2 border-transparent hover:border-blue-300 hover:text-gray-100"
+            class="border-b-2 hover:border-blue-300 hover:text-gray-100"
             :class="isUrl('about') ? 'border-blue-300 text-gray-100' : 'border-transparent'"
         >
             About
