edd6147f71e9a0f9b771b85ae25f6109bc81ec1f
Francisco Madeira <xico2k@gmail.com>
2021-12-03 23:10:54 +0000

    fix: Add more contrast

--- resources/js/Shared/MainFooter.vue
+++ resources/js/Shared/MainFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="px-8 my-12 w-full max-w-7xl text-xs text-white text-opacity-20 text-center">
+    <footer class="px-8 my-12 w-full max-w-7xl text-xs text-white text-opacity-50 text-center">
         © {{ year }}. No tracking or ads.
     </footer>
 </template>
