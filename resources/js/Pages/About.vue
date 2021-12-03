f545f9ecce30160fbbb0ea45f1015bfe569d2639
Francisco Madeira <xico2k@gmail.com>
2021-12-03 22:16:02 +0000

    fix: Changes on the about copy.

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,8 +1,8 @@
 <template>
     <Head title="About">
         <meta property="og:title" content="About - Francisco Madeira">
-        <meta property="og:description" content="I'm Francisco Madeira, 32 years old, I've been working as a Sofware Developer since 2010. Everyday I get to work with PHP, MySQL, TypeScript, JavaScript and I'm an HUGE fan of the Test-Driven Development principle, and I've been using it on all my projects.">
-        <meta name="description" content="I'm Francisco Madeira, 32 years old, I've been working as a Sofware Developer since 2010. Everyday I get to work with PHP, MySQL, TypeScript, JavaScript and I'm an HUGE fan of the Test-Driven Development principle, and I've been using it on all my projects.">
+        <meta property="og:description" content="I'm Francisco Madeira, 32 years old, I've been working as a Sofware Developer since 2010. Everyday I get to work with PHP, MySQL, TypeScript, JavaScript, and I'm an HUGE fan of the Test-Driven Development principle, and I've been using it on all my projects.">
+        <meta name="description" content="I'm Francisco Madeira, 32 years old, I've been working as a Sofware Developer since 2010. Everyday I get to work with PHP, MySQL, TypeScript, JavaScript, and I'm an HUGE fan of the Test-Driven Development principle, and I've been using it on all my projects.">
         <meta property="og:type" content="article">
         <meta property="og:url" content="https://franciscomadeira.com/about">
         <meta property="og:image" content="https://franciscomadeira.com/og-about.png">
@@ -22,7 +22,7 @@
             </h1>
         </div>
         <div class="text-base leading-6">
-            <p>I'm <b>Francisco Madeira</b>, <b>{{ age('1989-08-25') }}</b> years old, I've been working as a Sofware Developer since <b>2010</b>. Everyday I get to work with <b>PHP, MySQL, TypeScript, JavaScript</b> and I'm a HUGE fan of the <b>T</b>est-<b>D</b>riven <b>D</b>evelopment principle, and I've been using it on all my projects.</p>
+            <p>I'm <b>Francisco Madeira</b>, <b>{{ age('1989-08-25') }}</b> years old, I've been working as a Sofware Developer since <b>2010</b>. Everyday I get to work with <b>PHP, MySQL, TypeScript, JavaScript</b>, and I'm a HUGE fan of the <b>T</b>est-<b>D</b>riven <b>D</b>evelopment principle, and I've been using it on all my projects.</p>
             <p class="mt-8">
                 I had the chance to work with a lot of web tools in the past such as <b>Zend, CodeIgniter, Symfony, CakePHP, AngularJS, Ionic</b>, you name it...
             </p>
@@ -30,7 +30,7 @@
                 v-emoji
                 class="mt-8"
             >
-                Right now, I've been using the <b>❤️ Laravel Framework</b> and <b>🐳 Docker</b> almost on every project. For the frontend side, I've been using <b>VueJS</b> or <b>React</b> depending on the project needs.
+                Right now, I've been using the <b>⚡️ Laravel Framework</b> and <b>🐳 Docker</b> almost on every project. For the frontend side, I've been using <b>VueJS</b> or <b>React</b> depending on the project needs.
             </p>
             <p class="mt-8">
                 Also, I've been contributing to the Open-Source, on various projects, mostly, <b>PHP</b> and <b>JS</b> Packages, it's all available on my <a
@@ -51,7 +51,8 @@
                         class="text-gray-200 hover:text-white font-bold border-b-2 border-blue-300"
                         rel="noreferrer"
                         target="_blank"
-                    >🍃 Termwind</a>: In short, it's like Tailwind CSS, but for the <b>PHP command-line applications</b>.
+                        v-emoji
+                    >Termwind</a> 🍃: In short, it's like Tailwind CSS, but for the <b>PHP command-line applications</b>.
                 </li>
                 <li class="mt-4">
                     <a
@@ -67,9 +68,11 @@
                         class="text-gray-200 hover:text-white font-bold border-b-2 border-blue-300"
                         rel="noreferrer"
                         target="_blank"
-                    >MultiSort</a>: Allows you to sort a <b>multi dimensional</b> array easily.
+                    >MultiSort</a>: Allows you to sort a <b>multidimensional</b> array easily.
                 </li>
             </ul>
+
+            <p class="mt-8" v-emoji>Besides code, I'm also <b>Christian</b>, a <b>Guitar Player</b> and the boyfriend of <b>❤️ Inês</b>.</p>
         </div>
     </article>
 </template>
