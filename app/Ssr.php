ad687de469e57d9060f339417192f19dab80336e
Francisco Madeira <xico2k@gmail.com>
2021-12-04 17:09:08 +0000

    refactor: Cleanup Ssr Class

--- app/Ssr.php
+++ app/Ssr.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Cache;
 
 final class Ssr
 {
+    /**
+     * Gets the static version for the page.
+     *
+     * @param array<string, mixed> $page
+     * @return string|array<string, mixed>
+     */
     public function get(array $page, string $item = null): array|string
     {
         $key = $page['component'] . $page['version'];
@@ -16,13 +22,18 @@ final class Ssr
             Cache::add($key, $data = $this->exec($page));
         }
 
-        return $item ? $data[$item] : $data;
+        return $item && array_key_exists($item, $data) ? $data[$item] : $data;
     }
 
+    /**
+     * Executes the node ssr script to get the static information for the page.
+     *
+     * @return array<string, mixed>
+     */
     public function exec(array $page): array
     {
         $output = exec(sprintf("node %s '%s'", public_path('js/ssr.js'), json_encode($page)));
 
-        return json_decode($output !== false ? $output : '[]' , true);
+        return json_decode($output !== "" && $output !== false ? $output : '[]' , true);
     }
 }
