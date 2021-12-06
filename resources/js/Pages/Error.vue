7981c7b517161330007dfbc9ee885f1569cf5146
Francisco Madeira <xico2k@gmail.com>
2021-12-06 16:42:31 +0000

    feat: Add Inertia Error Handler

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-lg sm:text-2xl md:text-4xl lg:text-5xl font-bold text-center mt-8 mb-8">{{ title }}</h1>
+    <div class="text-center">{{ description }}</div>
+</template>
+
+<script setup>
+const props = defineProps({ status: Number });
+const title = {
+    503: '503: Service Unavailable',
+    500: '500: Server Error',
+    404: '404: Page Not Found',
+    403: '403: Forbidden',
+  }[props.status];
+
+const description = {
+    503: 'Sorry, we are doing some maintenance. Please check back soon.',
+    500: 'Whoops, something went wrong on our servers.',
+    404: 'Sorry, the page you are looking for could not be found.',
+    403: 'Sorry, you are forbidden from accessing this page.',
+}[props.status];
+</script>
