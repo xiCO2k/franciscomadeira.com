3e172330fca41a1124c239fa34b892da8acd4381
Francisco Madeira <xico2k@gmail.com>
2021-12-06 12:17:13 +0000

    fix: Add Missing Docblock

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,13 @@
 use App\Ssr;
 
 if (! function_exists('ssr')) {
+    /**
+     * Gets the static version for the page.
+     *
+     * @param array<string, mixed> $page
+     *
+     * @return string|array<string, mixed>
+     */
     function ssr(array $page, string $item = null): array|string
     {
         return app(Ssr::class)->get($page, $item);
