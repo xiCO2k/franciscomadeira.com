15f6e21942dea1868b7e044afa785a843d502998
Francisco Madeira <xico2k@gmail.com>
2021-12-08 14:36:10 +0000

    chore: Fix tests, to run without mix.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,11 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use LazilyRefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMix();
+    }
 }
