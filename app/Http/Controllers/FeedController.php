07193223d8edc3587c2f67dea2ffe6762919b4b8
Francisco Madeira <xico2k@gmail.com>
2022-04-08 15:29:21 +0100

    fix: Convert Markdown to HTML

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -24,11 +24,11 @@ class FeedController extends Controller
                 'title' => $post->title,
                 'link' => route('post.detail', $post),
                 'summary' => $post->description,
-                'content' => str_replace(
+                'content' => str()->markdown(str_replace(
                     ['```vue', '```blade'],
                     ['```js', '```php'],
                     preg_replace("/(```\w+)(,[^\s]+)/", '$1', $post->text)
-                ),
+                )),
                 'updated' => $post->updated_at->format('c'),
             ]));
 
