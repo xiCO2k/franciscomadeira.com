b85be56b98bee95e52eb244f978e8655c3949932
Francisco Madeira <xico2k@gmail.com>
2022-04-08 21:01:26 +0100

    static: fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,21 +22,13 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
+    /** @var array<int, string> */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
+    /** @var array<string, string> */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
